b0ec541491fb8c58ce71737ad49165cf3789c3a4
Steven Mirabito <steven@stevenmirabito.com>
2016-01-04 11:54:22 -0500

    Add custom error handlers

--- index.php
+++ index.php
@@ -14,9 +14,33 @@ use Psr\Http\Message\ResponseInterface as Response;
 $container = new \Slim\Container();
 
 // Add the Twig service to the container
-$container['twig'] = function($container) {
+$container['twig'] = function ($container) {
     $loader = new Twig_Loader_Filesystem('templates');
     return new Twig_Environment($loader, ['cache' => 'cache']);
+};
+
+// Add our custom error handler to the container
+$container['errorHandler'] = function ($container) {
+    return function ($request, $response, $exception) use ($container) {
+        // Load the error page template
+        $template = $container['twig']->loadTemplate('error.twig');
+        return $container['response']
+            ->withStatus(500)
+            ->withHeader('Content-Type', 'text/html')
+            ->write($template->render(['code' => 500]));
+    };
+};
+
+// Add our custom not found handler to the container
+$container['notFoundHandler'] = function ($container) {
+    return function ($request, $response) use ($container) {
+        // Load the error page template
+        $template = $container['twig']->loadTemplate('error.twig');
+        return $container['response']
+            ->withStatus(404)
+            ->withHeader('Content-Type', 'text/html')
+            ->write($template->render(['code' => 404]));
+    };
 };
 
 // Create and configure Slim app
@@ -39,18 +63,46 @@ $app->add(function (Request $request, Response $response, callable $next) {
     return $next($request, $response);
 });
 
+// Capture 404 and 500 responses and redirect them to our custom error handlers
+$app->add(function ($request, $response, $next) use ($container) {
+    // First execute anything else
+    $response = $next($request, $response);
+
+    // Check if the response should be handled by one of our custom error handlers
+    if (!$response->getBody()->getSize()) {
+        if ($response->getStatusCode() === 404) {
+            // Pass the request to the notFoundHandler
+            return $container['notFoundHandler']($request, $response);
+        } else if ($response->getStatusCode() === 500) {
+            // Pass the request to the errorHandler
+            return $container['errorHandler']($request, $response);
+        }
+    }
+
+    // Pass on current response for any other request
+    return $response;
+});
+
 /*
  * Slim Routes
  */
 
-// Wildcard route for GET requests
-$app->get('/[{path:.*}]', function ($request, $response, $args) {
-    // Load the template for the requested page
-    $template = $this->twig->loadTemplate('index.twig');
+// Wildcard route for GET requests (allows alphanumeric, forward slash, dash, and underscore)
+$app->get('/[{path:[a-zA-Z0-9\/\-\_]+}]', function ($request, $response, $args) {
+    // If $path isn't set, set it to 'index' to load the homepage
+    if (!isset($args['path'])) {
+        $args['path'] = 'index';
+    }
 
-    // Render the template and return it to the app
-    return $response->write($template->render(['name' => 'index']));
-    //return $response->write("Hi!");
+    // See if there's a template for the path requested
+    if (file_exists(sprintf("templates/%s.twig", $args['path']))) {
+        // Load the template for the requested page
+        $template = $this->twig->loadTemplate(sprintf("%s.twig", $args['path']));
+        return $response->write($template->render(['name' => basename($args['path'])]));
+    } else {
+        // Return a 404
+        return $response->withStatus(404);
+    }
 });
 
 $app->post('/contact', function ($request, $response, $args) {
